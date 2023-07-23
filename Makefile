f64447dd8d8c25b15c1347ff399361600f80667d
yuanczx <yuanczx@foxmail.com>
2023-07-23 16:08:21 +0800

    update: Makefile
    add: sort/quick sort
    
    fix: Makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 all: 
 	cd stack && $(MAKE)
 	cd sort && $(MAKE)
+
+.PHONY: clean
+
+clean:
+	@find "." -wholename "*/out/*" -exec rm {} \;
--- sort/src/test.c
+++ sort/src/test.c
@@ -1,6 +1,7 @@
 #include "bubble_sort.h"
 #include "insert_sort.h"
 #include "select_sort.h"
+#include "quick_sort.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -12,21 +13,28 @@ int main(int argc, char *argv[]) {
   printf("Select sort:\n");
   insert_sort(temp, length);
   for (int i = 0; i < length; i++) {
-    printf("%d, ", array[i]);
+    printf("%d, ", temp[i]);
   }
 
   memcpy(temp, array, sizeof(array));
   printf("\nBubble sort:\n");
   insert_sort(temp, length);
   for (int i = 0; i < length; i++) {
-    printf("%d, ", array[i]);
+    printf("%d, ", temp[i]);
   }
 
   memcpy(temp, array, sizeof(array));
   printf("\nSelect sort:\n");
   select_sort(temp, length);
   for (int i = 0; i < length; i++) {
-    printf("%d, ", array[i]);
+    printf("%d, ", temp[i]);
+  }
+
+  memcpy(temp, array, sizeof(array));
+  printf("\nQuick sort:\n");
+  quick_sort(temp,0, length-1);
+  for (int i = 0; i < length; i++) {
+    printf("%d, ", temp[i]);
   }
   return 0;
 }
